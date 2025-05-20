58d5e0af0b43fdd8f7a62d5437d8493f971df6fb
Abhilash Shetty K <abhilashsettyk@gmail.com>
2025-05-20 22:58:52 +0530

    Added constants

---
+++ src/utils/constants.js
@@ -0,0 +1,20 @@
+export const LOGO = "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const PROFILE_PIC = "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117";
+
+export const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YjM4ZTdkYTNhZjBmZDViZTBmMThlNzY3MzNhNzZlOSIsIm5iZiI6MS43NDY5NjEzNTk3Nzc5OTk5ZSs5LCJzdWIiOiI2ODIwODNjZmE5YmNlMGU1ODY3MzhiZWEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.yqq4CQg1-fjcRqpkIdD9EpaTSScydkaKMm8lsydpk3M'
+  }
+};
+
+export const POSTER_URL = "https://image.tmdb.org/t/p/w500/"
+
+export const SUPPORTED_LANGUAGES = [
+  {identifier: "en", language: "English"},
+  {identifier: "kannada", language: "kannada"},
+  {identifier: "hindi", language: "Hindi"},
+  {identifier: "spanish", language: "Spanish"}
+]
