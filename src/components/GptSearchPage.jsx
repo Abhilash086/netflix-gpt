4724225a194be5c883dba2091af5c46911ea6d37
Abhilash Shetty K <abhilashsettyk@gmail.com>
2025-05-23 02:42:01 +0530

    modified and made better GPT search page

--- src/components/GptSearchPage.jsx
+++ src/components/GptSearchPage.jsx
@@ -4,7 +4,7 @@ import GptSearchBar from "./GptSearchBar";
 const GptSearchPage = () => {
   return (
     <div className="w-full h-full bg-black/50">
-      <div className="h-screen -z-10 opacity-50 bg-center bg-cover absolute w-full bg-[url(https://assets.nflxext.com/ffe/siteui/vlv3/cb17c41d-6a67-4472-8b91-cca977e65276/web/IN-en-20250505-TRIFECTA-perspective_03ae1a85-5dcf-4d20-a8a6-1e61f7ef73cb_large.jpg)]"></div>
+      <div className="h-screen -z-10 bg-center bg-cover absolute w-full bg-[url(https://assets.nflxext.com/ffe/siteui/vlv3/cb17c41d-6a67-4472-8b91-cca977e65276/web/IN-en-20250505-TRIFECTA-perspective_03ae1a85-5dcf-4d20-a8a6-1e61f7ef73cb_large.jpg)]"></div>
       <div className="w-full h-auto pt-[20%] sm:pt-[10%] flex flex-col px-4 bg-black/50">
         <GptSearchBar />
         <GPTMovieSuggestions />
